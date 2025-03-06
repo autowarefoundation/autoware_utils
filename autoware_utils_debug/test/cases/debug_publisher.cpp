aa690be2f6f164eef824963b0cb3348b6005e01a
Takagi, Isamu <isamu.takagi@tier4.jp>
2025-03-06 20:14:17 +0900

    update test
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- autoware_utils_debug/test/cases/debug_publisher.cpp
+++ autoware_utils_debug/test/cases/debug_publisher.cpp
@@ -15,28 +15,45 @@
 #include "autoware_utils_debug/debug_publisher.hpp"
 
 #include <autoware_utils_testing/mock_node.hpp>
+#include <autoware_utils_testing/spin_thread.hpp>
 
 #include <gtest/gtest.h>
 
 #include <memory>
+#include <thread>
+
+void sleep(double seconds)
+{
+  const auto ms = static_cast<int64_t>(seconds * 1000);
+  std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
 
 TEST(TestDebugPublisher, Main)
 {
+  namespace DebugMsgs = autoware_internal_debug_msgs::msg;
+
   const auto mock = std::make_shared<autoware_utils_testing::MockNode>("mock_node");
   const auto node = std::make_shared<rclcpp::Node>("test_node");
+  auto pub = autoware_utils_debug::DebugPublisher(node.get(), "debug");
+  auto sub1 = mock->sub<DebugMsgs::Float64Stamped>("/debug/foo", rclcpp::QoS(5));
+  auto sub2 = mock->sub<DebugMsgs::StringStamped>("/debug/bar", rclcpp::QoS(5));
 
-  auto pub = autoware_utils_debug::DebugPublisher(node.get(), "foo/bar");
-  pub.publish<autoware_internal_debug_msgs::msg::Float64Stamped>("aaa", 123.456);
+  autoware_utils_testing::SpinThread thread;
+  thread.add_node(mock);
+  thread.add_node(node);
+  thread.start();
+  pub.publish<DebugMsgs::Float64Stamped>("foo", 12.34);
+  pub.publish<DebugMsgs::Float64Stamped>("foo", 56.78);
+  pub.publish<DebugMsgs::StringStamped>("bar", "hello");
+  pub.publish<DebugMsgs::StringStamped>("bar", "world");
+  sleep(0.01);
+  thread.stop();
 
-  rclcpp::executors::SingleThreadedExecutor executor;
-  executor.add_node(mock);
-  executor.add_node(node);
+  EXPECT_EQ(sub1->data.size(), 2);
+  EXPECT_EQ(sub1->data[0].data, 12.34);
+  EXPECT_EQ(sub1->data[1].data, 56.78);
 
-  std::thread thread([&executor] { executor.spin(); });
-  while (rclcpp::ok() && !executor.is_spinning()) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  }
-  std::this_thread::sleep_for(std::chrono::seconds(5));
-  executor.cancel();
-  thread.join();
+  EXPECT_EQ(sub2->data.size(), 2);
+  EXPECT_EQ(sub2->data[0].data, "hello");
+  EXPECT_EQ(sub2->data[1].data, "world");
 }
