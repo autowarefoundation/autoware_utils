31d48c25b440c1e03f850c76843942e8ee68f99d
Takagi, Isamu <isamu.takagi@tier4.jp>
2025-03-06 21:29:20 +0900

    remove testing
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- autoware_utils_debug/test/cases/debug_publisher.cpp
+++ autoware_utils_debug/test/cases/debug_publisher.cpp
@@ -14,46 +14,14 @@
 
 #include "autoware_utils_debug/debug_publisher.hpp"
 
-#include <autoware_utils_testing/mock_node.hpp>
-#include <autoware_utils_testing/spin_thread.hpp>
-
 #include <gtest/gtest.h>
 
 #include <memory>
-#include <thread>
 
-void sleep(double seconds)
+using autoware_utils_debug::DebugPublisher;
+
+TEST(TestDebugPublisher, Instantiation)
 {
-  const auto ms = static_cast<int64_t>(seconds * 1000);
-  std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-}
-
-TEST(TestDebugPublisher, Main)
-{
-  namespace DebugMsgs = autoware_internal_debug_msgs::msg;
-
-  const auto mock = std::make_shared<autoware_utils_testing::MockNode>("mock_node");
   const auto node = std::make_shared<rclcpp::Node>("test_node");
-  auto pub = autoware_utils_debug::DebugPublisher(node.get(), "debug");
-  auto sub1 = mock->sub<DebugMsgs::Float64Stamped>("/debug/foo", rclcpp::QoS(5));
-  auto sub2 = mock->sub<DebugMsgs::StringStamped>("/debug/bar", rclcpp::QoS(5));
-
-  autoware_utils_testing::SpinThread thread;
-  thread.add_node(mock);
-  thread.add_node(node);
-  thread.start();
-  pub.publish<DebugMsgs::Float64Stamped>("foo", 12.34);
-  pub.publish<DebugMsgs::Float64Stamped>("foo", 56.78);
-  pub.publish<DebugMsgs::StringStamped>("bar", "hello");
-  pub.publish<DebugMsgs::StringStamped>("bar", "world");
-  sleep(0.01);
-  thread.stop();
-
-  EXPECT_EQ(sub1->data.size(), 2);
-  EXPECT_EQ(sub1->data[0].data, 12.34);
-  EXPECT_EQ(sub1->data[1].data, 56.78);
-
-  EXPECT_EQ(sub2->data.size(), 2);
-  EXPECT_EQ(sub2->data[0].data, "hello");
-  EXPECT_EQ(sub2->data[1].data, "world");
+  DebugPublisher(node.get(), "namespace");
 }
--- autoware_utils_debug/test/main.cpp
+++ autoware_utils_debug/test/main.cpp
@@ -12,11 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <autoware_utils_testing/gtest/ros_env.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <gtest/gtest.h>
+
+class RclcppEnvironment : public testing::Environment
+{
+public:
+  RclcppEnvironment(int argc, char ** argv) : argc(argc), argv(argv) {}
+  void SetUp() override { rclcpp::init(argc, argv); }
+  void TearDown() override { rclcpp::shutdown(); }
+
+private:
+  int argc;
+  char ** argv;
+};
 
 int main(int argc, char ** argv)
 {
   testing::InitGoogleTest(&argc, argv);
-  testing::AddGlobalTestEnvironment(autoware_utils_testing::gtest::ros_env(argc, argv));
+  testing::AddGlobalTestEnvironment(new RclcppEnvironment(argc, argv));
   return RUN_ALL_TESTS();
 }
